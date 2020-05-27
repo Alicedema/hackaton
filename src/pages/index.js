f9a067cc1d8c245efa0b994dcc9f866077b8e6d2
A-De-Mauro <demauroalice@gmail.com>
2020-05-27 17:30:14 +0200

    remove unused CSS, title

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import Scrollspy from 'react-scrollspy';
 
 import Layout from '../components/Layout';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Scroll from '../components/Scroll';
-import overLayPic from '../assets/images/overlay.png';
-import { Link } from 'gatsby';
 
 // OUR GIFS
 import slowEthan from '../assets/images/slow_ethan.gif';
@@ -26,8 +21,6 @@ const sections = [
 ];
 const IndexPage = () => (
   <Layout>
-    <Header />
-
     <section id="one" className="main special">
       <div className="container">
         <img style={{ position: 'absolute', top: 0 }} width={250} height={250} src={coffeeStain1} alt="" />
@@ -36,22 +29,27 @@ const IndexPage = () => (
         </span>
         <div className="content">
           <header className="major">
+            <h1>The A-Team</h1>
           </header>
-          <p>
-          </p>
-          <p>
+          <div>            
+            <img src={niels} alt="" />
+            </div>
+          <div>            
             <img src={allTogether} alt="" />
-          </p>
-          <p>
-            <img src={slowEthan} alt="" />
-            <img src={alice} alt="" />
-          </p>
-          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div> <img src={slowEthan} alt="" /></div>
+          <div>            <img src={alice} alt="" />
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <img src={aliceNadia} alt="" />
-          </p>
-          <p>
-          <img src={niels} alt="" />
-        </p>
+          </div>
+          <div>
+          </div>
         </div>
         {/* <Scroll type="id" element="two">
           <a href="#two" className="goto-next ">
@@ -59,34 +57,8 @@ const IndexPage = () => (
           </a>
         </Scroll> */}
       </div>
-      <img style={{ position: 'absolute', top: 900, right: 200 }} width={400} height={350} src={coffeeStain2} alt="" />
+      <img style={{ position: 'absolute', top: 900, right: 250 }} width={400} height={350} src={coffeeStain2} alt="" />
     </section>
-
-    <section>
-      <div className="container">
-        <header className="major">
-        </header>
-      </div>
-      <Footer />
-    </section>
-
-    <Scrollspy
-      items={sections.map(s => s.id)}
-      currentClassName="active"
-      offset={50}
-      componentTag={'div'}
-    >
-      {sections.map(s => {
-        return (
-          <div
-            key={s.id}
-            className="main-bg"
-            id={`${s.id}-bg`}
-            style={{ backgroundImage: `url(${overLayPic}), url(${s.bg})` }}
-          />
-        );
-      })}
-    </Scrollspy>
   </Layout>
 );
 
